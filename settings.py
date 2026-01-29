9244c17ea34197e168a76109aac8bcac7d284e70
沉香Zone <cat210360@gmail.com>
2026-01-29 23:03:53 +0800

    武装飞船

---
+++ settings.py
@@ -0,0 +1,18 @@
+class Settings:
+    """储存游戏《外星人入侵》中所有设置的类"""
+    def __init__(self):
+        """初始化游戏的设置"""
+        #屏幕设置
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (0,0,0)
+
+        #飞船设置
+        self.ship_speed = 1.5
+        
+        #子弹设置
+        self.bullet_speed = 2.0
+        self.bullet_width = 3
+        self.bullet_height = 6
+        self.bullet_color = (255,215,0)
+        self.bullets_allowed = 3
